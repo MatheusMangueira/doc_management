8ba4b50a8b4b1d287936602a3ce041c4044ac1c5
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-05-04 16:03:05 -0300

    feat: create router for roles

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user/UserController";
+import { RolesController } from "../controllers/roles/RolesController";
 
 
 export class AllRouter {
@@ -15,8 +16,17 @@ export class AllRouter {
       this.router.post("/user", UserController.create);
    }
 
+   private rolesRouter() {
+      this.router.post("/roles", RolesController.create);
+      this.router.get("/roles", RolesController.getAll);
+      this.router.get("/roles/:id", RolesController.getById);
+      this.router.put("/roles/:id", RolesController.update);
+      this.router.delete("/roles/:id", RolesController.delete);
+   }
+
    private setupRoutes() {
       this.userRouter();
+      this.rolesRouter();
    }
 
    public getRouter(): Router {
