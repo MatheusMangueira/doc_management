ff5f3684df2692987e142cf9f011541f7549bb18
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-05-05 17:04:52 -0300

    feat: instence session

--- src/server/shared/factorys/index.ts
+++ src/server/shared/factorys/index.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "../../config/db";
 import { RolesModel, UserModel } from "../../models";
 import { UserService } from "../services";
 import { RolesService } from "../services/roles/RolesService";
+import { SessionService } from "../services/session/SessionService";
 
 
 export const userServiceIntance = new UserService(
@@ -10,4 +11,8 @@ export const userServiceIntance = new UserService(
 
 export const rolesServiceIntance = new RolesService(
    AppDataSource.getRepository(RolesModel)
+);
+
+export const sessionServiceIntance = new SessionService(
+   AppDataSource.getRepository(UserModel)
 );
