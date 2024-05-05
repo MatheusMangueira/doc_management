a9418ce1982a112231413180cecf39f90edd6f36
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-05-05 17:05:40 -0300

    feat: create router login

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user/UserController";
 import { RolesController } from "../controllers/roles/RolesController";
+import { SessionController } from "../controllers/session/SessionController";
+import { authenticated } from "../shared/middlewares/authenticated/authenticated";
+import { Permissions } from "../shared/middlewares";
 
 
 export class AllRouter {
@@ -18,15 +21,25 @@ export class AllRouter {
 
    private rolesRouter() {
       this.router.post("/roles", RolesController.create);
-      this.router.get("/roles", RolesController.getAll);
+
+      this.router.get("/roles",
+         authenticated(),
+         Permissions.is(["admin"]),
+         RolesController.getAll);
+
       this.router.get("/roles/:id", RolesController.getById);
       this.router.put("/roles/:id", RolesController.update);
       this.router.delete("/roles/:id", RolesController.delete);
    }
 
+   private session() {
+      this.router.post("/login", SessionController.handle);
+   }
+
    private setupRoutes() {
       this.userRouter();
       this.rolesRouter();
+      this.session();
    }
 
    public getRouter(): Router {
